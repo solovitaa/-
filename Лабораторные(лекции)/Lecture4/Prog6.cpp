bd00b5cf0b93e0e5579405c846809a49598c7724
solovitaa <104010224+solovitaa@users.noreply.github.com>
2022-05-19 22:49:55 +0300

    Add files via upload

---
+++ Лабораторные(лекции)/Lecture4/Prog6.cpp
@@ -0,0 +1,40 @@
+﻿#include <sys/stat.h>
+
+#include <fcntl.h>
+
+#include <string.h>
+
+#include <stdio.h>
+
+#define NAMEDPIPE_NAME "myfyle" //название файла канала
+#define BUFSIZE 50
+
+int main(int argc, char ** argv) {
+	int fd;
+	int len;
+	char buf[BUFSIZE];
+
+	
+	if (mkfifo(NAMEDPIPE_NAME, 0777)) {
+		perror("mkfifo");
+		return 1;
+	}
+	printf("%s is created\n", NAMEDPIPE_NAME);
+
+	if ((fd = open(NAMEDPIPE_NAME, O_RDONLY)) <= 0) {
+		perror("open error:");
+		return 1;
+	}
+	printf("%s is opened\n", NAMEDPIPE_NAME);
+
+	do {
+		memset(buf, '\0', BUFSIZE);
+		if ((len = read(fd, buf, BUFSIZE - 1)) <= 0) {
+			perror("read error");
+			close(fd);
+			remove(NAMEDPIPE_NAME);
+			return 0;
+		}
+		printf("Incomming message (%d): %s\n", len, buf);
+	} while (1);
+}
